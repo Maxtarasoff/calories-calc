41fd28194df8d7658a63e11592c834ca39143137
Max <maxtarasoff@gmail.com>
2025-03-29 22:01:06 +0800

    create project

---
+++ src/test/java/com/example/calories_calc/CaloriesCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calories_calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloriesCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
